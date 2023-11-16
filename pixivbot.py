c010a85ce1cb0240c21677ee47f7bd7c43fdd30a
kiennguyenGH <ktnguyen4647@gmail.com>
2023-11-15 19:02:50 -0800

    Attach images to embed and print

--- pixivbot.py
+++ pixivbot.py
@@ -1,7 +1,5 @@
 import os
-import json
 import discord
-import locale
 import requests
 from pixivpy3 import *
 from dotenv import load_dotenv
@@ -16,24 +14,25 @@ REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 api.auth(refresh_token = REFRESH_TOKEN)
 COLOR = 0x009cff
 
-json_result = api.illust_recommended()
-
 @client.event
 async def on_ready():
-	print("Bot is starting...")
+	print("Pixiv Discord Bot")
 	print("-------------------")
-	
 
 @client.command()
 async def search(ctx, arg):
 	json_result = api.illust_detail(arg)
-	print(json_result)
+	#print(json_result)
 	illust = json_result.illust
-	response = requests.get(illust.image_urls['large'])
-	print(response)
+	img_url = illust.image_urls['large']
+	api.download(img_url, fname = "image.jpg")
 	embed = discord.Embed(title = illust.title, url = "https://pixiv.net/en/artworks/" + str(illust.id), description = illust.caption, color = COLOR)
 	embed.set_author(name = illust.user['name'], url = "https://pixiv.net/en/users/" +  str(illust.user['id']))
-	embed.set_image(url = illust.meta_single_page['original_image_url'])
-	await ctx.send(embed = embed)
-
+	embed.add_field(name = "Total Views", value = illust['total_view'], inline = True)
+	embed.add_field(name = "Total Bookmarks", value = illust['total_bookmarks'], inline = True)
+	with open('image.jpg', 'rb') as file:
+		image = discord.File(file)
+		embed.set_image(url = "attachment://image.jpg")
+		await ctx.send(embed = embed, file = image)
+	os.remove('image.jpg')
 client.run(TOKEN)
